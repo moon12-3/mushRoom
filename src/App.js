ce8750deb2e70d260c4cfac7ff21c5f185f0a833
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-07-11 10:15:36 +0900

    메인 오류 수정

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Header/>
       <ScrollToTop/>
       <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/mushRoom" element={<Main />} />
           <Route path="/record" element={<Record/>}/>
           <Route path="/thesis" element={<Thesis/>}/>
           <Route path="/game" element={<Game/>}/>
